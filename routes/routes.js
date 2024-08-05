92b81407ebabd3aa4dcd727a02d8b0a189b0ad16
VPVelthuizen <vpvelthuizen@gmail.com>
2024-08-05 12:47:56 -0400

    fixed routes

--- routes/routes.js
+++ routes/routes.js
@@ -5,22 +5,13 @@ const { v4: uuidv4 } = require('uuid');
 
 const router = express.Router();
 
-// HTML routes
-router.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
-});
-
-router.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-});
-
 // API routes
-router.get('/api/notes', (req, res) => {
+router.get('/notes', (req, res) => {
     const notes = JSON.parse(fs.readFileSync(path.join(__dirname, '../db/db.json'), 'utf8'));
     res.json(notes);
 });
 
-router.post('/api/notes', (req, res) => {
+router.post('/notes', (req, res) => {
     const newNote = req.body;
     newNote.id = uuidv4();
 
@@ -30,6 +21,17 @@ router.post('/api/notes', (req, res) => {
     fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(notes));
 
     res.json(newNote);
+});
+
+router.delete('/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+
+    let notes = JSON.parse(fs.readFileSync(path.join(__dirname, '../db/db.json'), 'utf8'));
+    notes = notes.filter(note => note.id !== noteId);
+
+    fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(notes));
+
+    res.sendStatus(204);
 });
 
 module.exports = router;
